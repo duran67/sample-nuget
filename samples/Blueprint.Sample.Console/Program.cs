e03310b55fd0e47311e797f4dd4b920857e76059
Bergman Johan <johan.bergman@kapsch.net>
2020-05-27 17:09:12 +0200

    added consumer app

---
+++ samples/Blueprint.Sample.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Blueprint.Sample.CSharpLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var world = new World();
+        var text = world.Hello();
+        Console.WriteLine(text);
+    }
+}
+
