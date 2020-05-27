7ce99507680b4cc955444e42304d476f5ff7e19d
bergman <johan.bergman@kapsch.net>
2020-05-27 16:05:48 +0200

    update build

--- build.ps1
+++ build.ps1
@@ -1,5 +1,1 @@
-Param([Parameter(Mandatory=$true)][string]$version)
-
-$ErrorActionPreference = "Stop"
-
-dotnet pack --nologo --output "$pwd\artifacts" --configuration Release /p:Version=$version /p:PackageVersion=$version
+dotnet pack --nologo --output "$pwd\artifacts" --configuration Release 
