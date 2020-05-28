b9e21f6c226c88144b92e274e41aa2a6113b8426
bergman <johan.bergman@kapsch.net>
2020-05-28 12:53:33 +0200

    Testing against json convert

--- samples/Blueprint.Sample.Console/Program.cs
+++ samples/Blueprint.Sample.Console/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Blueprint.Sample.CSharpLib;
+using Newtonsoft.Json;
 
 class Program
 {
     static void Main(string[] args)
     {
         var world = new World();
+        JsonConvert.SerializeObject(world, Formatting.Indented);
         var text = world.Hello();
         Console.WriteLine(text);
     }
